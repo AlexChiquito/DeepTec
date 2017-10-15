cf823fd695ea3951b3315aaa5605ee82489e3905
AlexChiquito <alejandrochi_257@hotmail.com>
2017-10-15 17:50:34 -0500

    izquierda!!!!

--- deepTec_control/src/lineController.cpp
+++ deepTec_control/src/lineController.cpp
@@ -4,6 +4,8 @@ lineController::lineController():it_(nh_){
 	ROS_INFO("Line follower");
 	lines_sub_ = nh_.subscribe<deepTec_control::LineArrayStamped>("/hough_lines/lines",1, &lineController::linesCB,this);
 	action_pub_ = nh_.advertise<std_msgs::String>("/command", 1);
+	x_reference = 480;
+	m_reference = -2.40;
 }
 
 lineController::~lineController(){
@@ -24,15 +26,40 @@ void lineController::linesCB(const deepTec_control::LineArrayStamped msg){
 
 
 float lineController::computeCross(float x1, float x2, float y1, float y2){
-	float m = (y2 - y1) / (x2 - x1);
+	m = (y2 - y1) / (x2 - x1);
 	float y = -300;
 	float result = (m*x1 - y1 + y) / m;
 	ROS_INFO_STREAM("m: "<<m);
-	ROS_INFO_STREAM("result: " << result);		
+	ROS_INFO_STREAM("result: " << result);
+	return result;		
 }
 
 void lineController::actionCompute(float x){
+	std_msgs::String msg;
+
+	if (m>(m_reference + 0.5) && m<0){
+		ROS_INFO_STREAM("izquierda");
+		msg.data = "left";
+	}else if (m<(m_reference - 0.5) || m>0){
+		ROS_INFO_STREAM("derecha");
+		msg.data = "right";
+	}else{
+		ROS_INFO_STREAM("derecho");
+		msg.data = "straight";
+	}
+
+	if (x>(x_reference + 30)){		//pendiente mayor que la referencia y menor que 0
+		ROS_INFO_STREAM("izquierda!");
+		msg.data = "left";		
+	}else if(x<(x_reference-30)){	//pendiente menor que la referencia o mayor que 0
+		ROS_INFO_STREAM("derecha");
+		msg.data = "right";
+	}else{
+		ROS_INFO_STREAM("derecho");
+	}
 	ROS_INFO_STREAM("Computing....");
+
+	action_pub_.publish(msg);
 }
 
 int main(int argc, char **argv){
