9efbe8b9d7d51a66ca8f592dbb96269c4e9df2c5
AlexChiquito <alejandrochi_257@hotmail.com>
2017-10-31 13:09:34 -0500

    detector de obstaculos added

--- deepTec_control/include/deepTec_control/lineController.h
+++ deepTec_control/include/deepTec_control/lineController.h
@@ -3,6 +3,7 @@
 #include <cv_bridge/cv_bridge.h>
 #include <deepTec_control/LineArrayStamped.h>
 #include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/LaserScan.h>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <std_msgs/String.h>
@@ -17,13 +18,16 @@ class lineController{
 	image_transport::ImageTransport it_;
 	image_transport::Subscriber im_sub_;
 	ros::Subscriber lines_sub_;
+	ros::Subscriber scan_sub_;
 	ros::Publisher action_pub_;
 
 	float m;
 	float x_reference;
 	float m_reference;
+	bool obstacle; 
 	
 	float computeCross(float x1, float x2, float y1, float y2);
 	void actionCompute(float x);
+	void scanCB(const sensor_msgs::LaserScan msg);
 	void linesCB(const deepTec_control::LineArrayStamped msg);
 };
